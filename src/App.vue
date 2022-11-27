9014ea592a6677a887035c0274d04e35a91bcb65
Danila Garus <danilagarus2003@gmail.com>
2022-11-27 14:00:19 +0300

    ...

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Datepicker></Datepicker>
+</template>
+
+<script>
+
+import Datepicker from "@/components/Datepicker";
+
+export default {
+  components: {Datepicker},
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Datepicker.vue
@@ -0,0 +1,31 @@
+<template>
+  <Datepicker v-model="date" :min-date="minDate" :max-date="maxDate" :clearable="false" highlight-disabled-days hide-offset-dates style="max-width: 150px"/>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+  setup() {
+
+    const date = ref();
+    const maxDate = new Date();
+    const minDate = new Date();
+
+    maxDate.setDate(maxDate.getDate() + 1);
+    minDate.setDate(10);
+
+    onMounted(() => {
+      const startDate = new Date();
+      const endDate = new Date(new Date().setDate(startDate.getDate()));
+      date.value = [startDate, endDate];
+    })
+
+    return {
+      date,
+      maxDate,
+      minDate,
+    }
+  },
+}
+</script>
